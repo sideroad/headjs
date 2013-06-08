98e5bc7defc8091ff5bfc7b437a6fa9ad9ab5695
sideroad <sideroad.jp@gmail.com>
2013-06-08 20:11:28 +0900

    Refactoring test case

--- test/unit/charset.js
+++ test/unit/charset.js
@@ -11,40 +11,31 @@
     asyncTest('load-different-encoding', 10, function () {
         var first = [
                 'charset-tests/1sjis.js#Shift_JIS',
-                'charset-tests/2utf8.js#UTF-8'
+                'charset-tests/2utf8.js#UTF-8',
+                function(){
+                    ok(true, 'head.ready');
+                    ok( sjis );
+                    equal( sjis.A, 'あ');
+                    ok( sjis.utf8 );
+                    equal( sjis.utf8.A, 'あ');
+                    head.js.apply(head, second);
+                }
             ],
             second = [
                 'charset-tests/3sjis.js#Shift_JIS',
-                'charset-tests/4utf8.js#UTF-8'
-            ];
-      
-      
-        head.js.apply( head, first );
-  
-        head.ready(function(){
-           ok(true, 'head.ready');
-           ok( sjis );
-           equal( sjis.A, 'あ');
-           ok( sjis.utf8 );
-           equal( sjis.utf8.A, 'あ');
-        });
-            
-      
-        setTimeout(function(){
-        
-            head.js.apply( head, second );
-    
-            head.ready(function(){
-                ok(true, 'head.ready');
-                ok( sjis.utf8.sjis );
-                equal( sjis.utf8.sjis.A, 'あ');
-                ok( sjis.utf8.sjis.utf8 );
-                equal( sjis.utf8.sjis.utf8.A, 'あ');
-                start();
-            });
+                'charset-tests/4utf8.js#UTF-8',
+                function(){
+                    ok(true, 'head.ready');
+                    ok( sjis.utf8.sjis );
+                    equal( sjis.utf8.sjis.A, 'あ');
+                    ok( sjis.utf8.sjis.utf8 );
+                    equal( sjis.utf8.sjis.utf8.A, 'あ');
+                    start();
 
-        }, 300);
-     
+                }
+            ];
+        expect(10);
+        head.js.apply(head, first);
     });
 
 
