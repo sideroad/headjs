6df2fe7de71a0c4e591a857377ac15b07deaf20b
sideroad <sideroad.jp@gmail.com>
2013-06-08 22:21:17 +0900

    Add testem, fixed version

--- test/unit/charset.js
+++ test/unit/charset.js
@@ -8,24 +8,32 @@
     'use strict';
     module('Load');
 
-    asyncTest('load-different-encoding', 10, function () {
+    asyncTest('load-different-encoding', function () {
         var first = [
                 'charset-tests/1sjis.js#Shift_JIS',
                 'charset-tests/2utf8.js#UTF-8',
                 function(){
-                    ok(true, 'head.ready');
                     ok( sjis );
                     equal( sjis.A, 'あ');
                     ok( sjis.utf8 );
                     equal( sjis.utf8.A, 'あ');
-                    head.js.apply(head, second);
                 }
             ],
             second = [
+                'charset-tests/1sjis.js#Shift_JIS',
+                'charset-tests/2utf8.js#UTF-8',
+                function(){
+                    ok( sjis );
+                    equal( sjis.A, 'あ');
+                    ok( sjis.utf8 );
+                    equal( sjis.utf8.A, 'あ');
+                    head.js.apply(head, third);
+                }
+            ],
+            third = [
                 'charset-tests/3sjis.js#Shift_JIS',
                 'charset-tests/4utf8.js#UTF-8',
                 function(){
-                    ok(true, 'head.ready');
                     ok( sjis.utf8.sjis );
                     equal( sjis.utf8.sjis.A, 'あ');
                     ok( sjis.utf8.sjis.utf8 );
@@ -34,10 +42,10 @@
 
                 }
             ];
-        expect(10);
+        expect(12);
         head.js.apply(head, first);
+        head.js.apply(head, second);
     });
-
 
 })(jQuery);
 
